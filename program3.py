f3320c4cfa8aba0017f5fccfedf5a7d244107427
Samira <54632872+S-samira2020@users.noreply.github.com>
2020-09-27 01:00:57 +0600

    Add files via upload

---
+++ program3.py
@@ -0,0 +1,13 @@
+# 5.2
+
+names  = ['samira', 'sakiba', 'babu']
+for name in names:
+    if name  == 'babu':
+        print(name)
+
+car  = 'Audi'
+car == 'audi'   # it will be false coz value assigned upper case and comparison is lower case
+
+car  = 'Audi'
+car.lower() == 'audi'
+
---
+++ test.py
@@ -0,0 +1,26 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> car  = 'Audi'
+>>> car  == 'audi'
+False
+>>> car.upper()
+'AUDI'
+>>> car.lower()  == 'audi'
+True
+>>> a = 22
+>>> b  = 22
+>>> a == b
+True
+>>> a > b
+False
+>>> a < b
+False
+>>> a >= b
+True
+>>> a<= b
+True
+>>> a and b
+22
+>>> a >= b and b >= a
+True
+>>> 
